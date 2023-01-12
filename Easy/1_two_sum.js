e9bcd990ee9ebb61710d03e44f51ad5e932d0413
sumanghosh <suman.ghosh@navi.com>
2023-01-13 00:11:56 +0530

    blind 75 leetcode array solutions

---
+++ Easy/1_two_sum.js
@@ -0,0 +1,14 @@
+var twoSum = function(nums, target) {
+    var map = {};
+    for(let i = 0; i< nums.length;i++){
+        let value = nums[i];
+        let complementPair = target - value;
+        // if complementPair is present in map
+        if(map[complementPair] !== undefined){
+            return [map[complementPair], i]
+        }else{
+            map[value] = i;
+        }
+    }
+    
+};
---
+++ Easy/217_contains_duplicates.js
@@ -0,0 +1,12 @@
+var containsDuplicate = function(nums) {
+    let count = 0;
+    let map = {};
+    for(let i = 0; i<nums.length; i++){
+        if(map[nums[i]] === undefined){
+            map[nums[i]] = i;
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
